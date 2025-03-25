4aef84edf9227e657e011a849cb9e71ffc201346
Leonardo Garcia <leonardogarcia@sescrio.org.br>
2025-03-25 14:18:49 +0000

    First commit...

---
+++ src/main/webapp/js/blockly/LogicaProgWeb.blockly.js
@@ -0,0 +1,20 @@
+window.blockly = window.blockly || {};
+window.blockly.js = window.blockly.js || {};
+window.blockly.js.blockly = window.blockly.js.blockly || {};
+window.blockly.js.blockly.LogicaProgWeb = window.blockly.js.blockly.LogicaProgWeb || {};
+
+/**
+ * @function Executar
+ *
+ *
+ *
+ * @param entradaUsuario
+ *
+ * @author Leonardo Garcia
+ * @since 25/03/2025, 10:59:42
+ *
+ */
+window.blockly.js.blockly.LogicaProgWeb.ExecutarArgs = [{ description: 'entradaUsuario', id: '7d6f3a6b' }];
+window.blockly.js.blockly.LogicaProgWeb.Executar = async function(entradaUsuario) {
+
+}
